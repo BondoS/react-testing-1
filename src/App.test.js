c8dd0266a5fd36f0b96004f0334075372deb3c8e
BondoS <eng.serag00@gmail.com>
2019-12-03 13:50:23 +0100

    find, toBe, children, hasClass

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import expectExport from 'expect';
 
 configure({ adapter: new Adapter() });
 
 describe('<App />', ()=> {
-  it('should render App', () => {
+  it('should contain p element', () => {
     const wrapper = shallow(<App />);
-    console.log(wrapper.debug());
+    expectExport(wrapper.find('.App-header').children().length).toBe(3);
   })
 })
